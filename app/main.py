a65499324bd2768d26bf9b46814af873250e7f05
Oleg <ototskiy@gmail.com>
2026-02-07 22:16:02 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -8,6 +8,8 @@ def move_file(command: str) -> None:
     linux_command, source_file, destination_file = command_in_list
     if linux_command != "mv":
         return
+    if destination_file[-1] == "/":
+        destination_file += source_file
     path = command_in_list[2].split("/")
     for element_of_path in range(len(path)):
         if element_of_path == len(path) - 1 and path[element_of_path]:
